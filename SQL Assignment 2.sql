c731e8fe2a94b09d43a7890c63df4e973b2c3866
mrYadav2079 <89142226+mrYadav2079@users.noreply.github.com>
2022-03-09 20:58:28 +0530

    Update SQL Assignment 2.sql

--- SQL Assignment 2.sql
+++ SQL Assignment 2.sql
@@ -1,9 +1,12 @@
+//question 1
+
 SELECT count(film.film_id) as No_of_Movie_with_dlt_secen 
 FROM film 
 INNER JOIN film_category ON film.film_id=film_category.film_id
 INNER JOIN category ON category.category_id=film_category.category_id
 WHERE category.name='Documentary' AND film.special_features='deleted scenes';
 
+//question 2
 
 SELECT count(film_category.film_id)as No_Stf_Movie 
 FROM film_category
@@ -13,10 +16,13 @@ INNER JOIN store ON store.store_id=inventory.store_id
 INNER JOIN staff ON staff.staff_id=store.manager_staff_id
 WHERE category.name='sci-fi' AND staff.first_name='jon' AND staff.last_name='Stephens';
 
+//question 3
 
 SELECT total_sales 
 FROM sales_by_film_category
 WHERE category='Animation';
+
+question 4
 
 SELECT category.name, count(customer.customer_id) AS NO_OF_CUSTOMER 
 FROM customer
@@ -27,6 +33,8 @@ INNER JOIN category ON category.category_id=film_category.category_id
 WHERE customer.first_name='PATRICIA' AND customer.last_name='JOHNSON'
 GROUP BY category.name 
 ORDER BY count(customer.customer_id) DESC LIMIT 3;
+
+question 5
 
 SELECT count(film.rating) AS NO_OF_R_RATED_MOVIES 
 FROM film
